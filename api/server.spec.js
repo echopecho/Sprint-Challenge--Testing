6c262b4535766926eab392ed79aeb697eb53c080
echopecho <pbsmithtbn@gmail.com>
2019-04-20 11:44:50 -0500

    Confirm testing environment is working

---
+++ api/server.spec.js
@@ -0,0 +1,10 @@
+const server = require('./server.js');
+const request = require('supertest');
+
+describe('the server', () => {
+
+  it('should run the test database', () => {
+    const env = process.env.DB_ENV;
+    expect(env).toBe('testing');
+  })
+})

7e0998dce3020c12a02e8731729c968522e488d7
echopecho <pbsmithtbn@gmail.com>
2019-04-20 11:39:13 -0500

    Finish api set up in preparation for testing

---
+++ games/gamesModel.js
@@ -0,0 +1,14 @@
+const db = require('../data/dbConfig.js');
+
+module.exports = {
+  find,
+  create,
+};
+
+async function find() { 
+  return null;
+};
+
+async function create() {
+  return null;
+};

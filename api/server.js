3fb58e9056389a40ae01d3ca512ca30baad0a8ba
echopecho <pbsmithtbn@gmail.com>
2019-04-20 12:34:55 -0500

    Fix api to cause second post test to pass

--- api/server.js
+++ api/server.js
@@ -6,8 +6,18 @@ const server = express();
 server.use(express.json());
 
 server.post('/', async (req, res) => {
-  const newGame = await Games.create(req.body);
-  res.status(201).json(newGame);
+  const { title, genre } = req.body;
+
+  if(title && genre) {
+    try {
+      const newGame = await Games.create(req.body);
+      res.status(201).json(newGame);
+    } catch(e) {
+      res.status(500).json({error: "Something went wrong with the server"})
+    }
+  } else {
+    res.status(422).json({message: "Please include both title and genre"})
+  }
 });
 
 module.exports = server;
